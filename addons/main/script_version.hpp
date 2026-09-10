a16a206c772a743268166654a931f51e9d6cee5f
Kartaviy <tonykarkfake@yandex.ru>
2026-09-10 09:28:22 +0400

    Изменена версия

--- addons/main/script_version.hpp
+++ addons/main/script_version.hpp
@@ -1,8 +1,8 @@
 #define MAJOR 1
 #define MINOR 4
-#define PATCH 0
+#define PATCH 1
 #define BUILD 0
 
 #define VERSION 1.4
-#define VERSION_STR 1.4.0.0
-#define VERSION_AR 1,4,0,0
+#define VERSION_STR 1.4.1.0
+#define VERSION_AR 1,4,1,0
